6edf44bb1ba38c09d8201cbc8f4470bf83e2d659
dlb3un <dlb3un@virginia.edu>
2015-02-28 15:27:22 -0500

    creating bug to spawn

---
+++ hackuvaunity/Assets/spawn.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+
+
+    var spawnTime : float = 5f;        // The amount of time between each spawn.
+    var spawnDelay : float = 3f;       // The amount of time before spawning starts.        
+    var enemies : GameObject[];        // Array of enemy prefabs.
+
+    function Start ()
+    {
+        // Start calling the Spawn function repeatedly after a delay .
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    function Spawn ()
+    {
+        // Instantiate a random enemy.
+        var enemyIndex : int = Random.Range(0, enemies.Length);
+        Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+    }
